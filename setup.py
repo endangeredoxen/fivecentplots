3e15928cc882f03675ec7b06ce85e2538e5fef37
Steve <endangeredoxen@users.noreply.github.com>
2018-06-19 09:15:29 -0600

    update fileio version

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pandas', 'numpy', 'matplotlib','fileio==0.1.0'],
+    install_requires=['pandas', 'numpy', 'matplotlib','fileio==0.2.2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
