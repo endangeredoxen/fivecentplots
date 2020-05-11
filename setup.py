7d3d47abac93a8613aa3c5c14ed4c274e095d148
Steve Nicholes <steve.nicholes@gmail.com>
2020-05-11 09:40:02 -0600

    setup file update for pypi

--- setup.py
+++ setup.py
@@ -22,13 +22,14 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.0',
+    version='0.3.1',
 
-    description='Wrapper for matplotlib to make plots the way I like them',
-    long_description=long_description,
+    description='Wrapper for python plotting across packages with easier syntax',
+    long_description='Wrapper for python plotting across packages with easier syntax',
 
     # The project's main homepage.
     url='https://github.com/endangeredoxen/fivecentplots',
+    download_url='https://github.com/endangeredoxen/fivecentplots/archive/v0.3.1.tar.gz',
 
     # Author details
     author='Steve Nicholes',
@@ -38,27 +39,27 @@ setup(
     license='GPLv3',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+    # classifiers=[
+    #     # How mature is this project? Common values are
+    #     #   3 - Alpha
+    #     #   4 - Beta
+    #     #   5 - Production/Stable
+    #     'Development Status :: 4 - Beta',
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Engineers/Scientists',
-        'Topic :: Data Analysis',
+    #     # Indicate who your project is intended for
+    #     'Intended Audience :: Engineers/Scientists',
+    #     'Topic :: Data Analysis',
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: GPL v3 License',
+    #     # Pick your license as you wish (should match "license" above)
+    #     'License :: GPL v3 License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-    ],
+    #     # Specify the Python versions you support here. In particular, ensure
+    #     # that you indicate whether you support Python 2, Python 3 or both.
+    #     'Programming Language :: Python :: 3',
+    # ],
 
     # What does your project relate to?
-    keywords='data',
+    keywords=['data', 'plotting'],
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
