6d9bdf182198583dd282b0662957afdca7ba3419
Steve Nicholes <steve.nicholes@gmail.com>
2023-02-17 13:04:46 -0700

    modify show_all test func

--- tests/test_boxplot.py
+++ tests/test_boxplot.py
@@ -53,7 +53,7 @@ def make_all():
         print('done!')
 
 
-def show_all():
+def show_all(only_fails=False):
     """
     Run the show=True option on all plt functions
     """
@@ -64,8 +64,15 @@ def show_all():
     members = [f for f in members if 'plt_' in f[0]]
     for member in members:
         print('Running %s...' % member[0], end='')
-        member[1](show=True)
-        db()
+        if only_fails:
+            try:
+                member[1]()
+            except AssertionError:
+                member[1](show=True)
+                db()
+        else:
+            member[1](show=True)
+            db()
 
 
 # plt_ functions can be used directly outside of pytest for debug
